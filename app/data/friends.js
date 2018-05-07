2928121d9b4bcad81c256c5dea162e20c49835e8
torajewel143 <mirzajewel1311@gmail.com>
2018-05-07 11:29:58 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+    name: "John Torpey",
+    photo: "https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Laura Toner",
+    photo: "https://www.pexels.com/photo/men-s-black-coat-with-white-polo-shirt-213117/",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Bob Merad",
+    photo: "https://www.pexels.com/photo/beard-blur-casual-close-up-462680/",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Amy Crosby",
+    photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Chris Maclin",
+    photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Anna Schultz",
+    photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Jack Waller",
+    photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
